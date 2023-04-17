336ed9fc83ffe7351bcd653ec066b2bdfac92c47
Rohita2002 <sairohita30032002@gmail.com>
2023-04-17 18:32:45 +0530

    added Admin login

---
+++ joyride-master/joyride/client/components/ViewUsers.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+/**
+ * The about page.
+ */
+export default class ViewUsers extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <div className="AdminHomePage">Hi Admin! View users</div>;
+	}
+}
