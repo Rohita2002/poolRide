9a37ab867eb1dfac3e5eb40bd4ecca35ff2100d9
Rohita2002 <sairohita30032002@gmail.com>
2023-04-17 23:08:28 +0530

    console logged pools and users for admin

--- joyride-master/joyride/client/components/ViewPools.jsx
+++ joyride-master/joyride/client/components/ViewPools.jsx
@@ -12,8 +12,8 @@ export default class ViewPools extends Component {
 		};
 
 		this.getEveryRide = this.getEveryRide.bind(this);
-		// this.handleDelete = this.handleDelete.bind(this);
-		// this.displayRides = this.displayRides.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+		this.displayRides = this.displayRides.bind(this);
 
 		this.getEveryRide();
 	}
@@ -39,58 +39,58 @@ export default class ViewPools extends Component {
 				data.forEach((ride) => {
 					arr.push(ride);
 				});
-				self.setState((state) => ({
+				self.setState({
 					Rides: arr,
-				}));
+				});
 				console.log('data', data);
 				console.log('rides in getting', this.state.Rides);
 			});
 	}
 
-	// handleDelete(id) {
-	// 	console.log('ride to be deleted:', id);
-	// }
+	handleDelete(id) {
+		console.log('ride to be deleted:', id);
 
-	// displayRides() {
-	// 	let rideGroups = [];
-	// 	if (this.state.Rides.length > 0) {
-	// 		for (let ride of this.state.Rides) {
-	// 			rideGroups.push(
-	// 				<tr key={ride._id}>
-	// 					<td>{index + 1}</td>
-	// 					<td>{ride.departure}</td>
-	// 					<td>{ride.destination}</td>
-	// 					<td>
-	// 						<button onClick={() => this.handleDelete(ride._id)}>
-	// 							Delete
-	// 						</button>
-	// 					</td>
-	// 				</tr>
-	// 			);
-	// 		}
-	// 		return rideGroups;
-	// 	} else {
-	// 		return <div id="NoRides">No rides yet!</div>;
-	// 	}
-	// }
+		const uri = `http://localhost:${process.env.PORT}/ride/${id}`;
+
+		self = this;
+
+		fetch(uri, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then( (response) => {
+				if (response.status === 200) {
+					window.location.reload();
+				}
+			})
+			.catch((err)=> {
+				console.log('Request failed', err);
+			});
+	}
+
+	displayRides() {
+		console.log('rides in display', this.state.Rides);
+		// console.log('rides in category', this.state.Rides[0].category);
+
+		// let rideGroups = [];
+		// if (this.state.Rides.length > 0) {
+		// 	for (let ride of this.state.Rides) {
+		// 		rideGroups.push(<p>{ride.category}</p>);
+		// 	}
+		// 	return rideGroups;
+		// } else {
+		// 	return <div id="NoRides">No rides yet!</div>;
+		// }
+
+		return null;
+	}
 
 	render() {
 		return (
 			<div>
-				{/* <table>
-					<thead>
-						<tr>
-							<th>Serial No.</th>
-							<th>Departure</th>
-							<th>Destination</th>
-							<th>Delete</th>
-						</tr>
-					</thead>
-					<tbody>
-						<this.displayRides />
-					</tbody>
-				</table> */}
-				viewing pools
+				<this.displayRides />
 			</div>
 		);
 	}
--- joyride-master/joyride/client/components/ViewUsers.jsx
+++ joyride-master/joyride/client/components/ViewUsers.jsx
@@ -6,9 +6,81 @@ import React, { Component } from 'react';
 export default class ViewUsers extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			users: null,
+		};
+
+		this.getAllUsers = this.getAllUsers.bind(this);
+		this.handleDelete = this.handleDelete.bind(this);
+		this.displayUsers = this.displayUsers.bind(this);
+
+		this.getAllUsers();
+	}
+
+	getAllUsers() {
+		const uri = `http://localhost:${process.env.PORT}/user/allusers`;
+
+		// Get user id and send it in with the post request.
+
+		self = this;
+
+		fetch(uri, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const arr = [];
+				data.forEach((user) => {
+					arr.push(user);
+				});
+				self.setState({
+					users: arr,
+				});
+				console.log('data', data);
+				console.log('users in getting', this.state.users);
+			});
+	}
+
+	handleDelete(id) {
+		console.log('user to be deleted:', id);
+
+		const uri = `http://localhost:${process.env.PORT}/user/${id}`;
+
+		self = this;
+
+		fetch(uri, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((response) => {
+				if (response.status === 200) {
+					window.location.reload();
+				}
+			})
+			.catch((err) => {
+				console.log('Request failed', err);
+			});
+	}
+
+	displayUsers() {
+		console.log('users in display', this.state.users);
+
+		return null;
 	}
 
 	render() {
-		return <div className="AdminHomePage">Hi Admin! View users</div>;
+		return (
+			<div>
+				<this.displayUsers />
+			</div>
+		);
 	}
 }
